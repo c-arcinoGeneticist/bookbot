9a2d6e20df4cc2a971fae5e3333861e563117ac8
Sam Crabtree <sam.crab94@gmail.com>
2026-07-30 17:57:34 -0500

    Add character counting and printing functionality

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from stats import word_count
+from stats import word_count, char_count
 
 def main():
     num_words = word_count("books/frankenstein.txt")
+    how_many_chars = char_count("books/frankenstein.txt")
     print(f"Found {num_words} total words")
+
+    for char, times in how_many_chars.items():
+        print(f"\'{char}\': {times}")
 
 main()
--- stats.py
+++ stats.py
@@ -7,3 +7,15 @@ def word_count(book):
     words = get_book_text("books/frankenstein.txt")
     split = words.split()
     return len(split)
+
+def char_count(text):
+    full_text = get_book_text(text)
+    chars = {}
+
+    for char in full_text.lower():
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+
+    return chars
